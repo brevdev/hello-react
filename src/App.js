83365a78186cb42ca33df2c45c6cfdbfe08c0dcb
nader <nader@brev.dev>
2022-03-16 18:02:14 +0000

    changes for microacquire

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import logo from "./brev.png";
+import logo from "./logo.svg";
+import microacquire_logo from "./microacquire.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="logos">
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This react app is ready to run!</p>
-        <p>Hot reload for a deployed app.</p>
+        <img src={microacquire_logo} className="App-logo ma" alt="logo" />
+        </div>
+        <p>Dear acquirer, this app is now yours 🎉</p>
         <a
           className="App-link"
           href="https://brev.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          our site
+          brev.dev
         </a>
       </header>
     </div>
